ff9eadb3439cc0513bfcfd03bcc34f688c4b8861
radu-bordea <radu.bordea.dev@gmail.com>
2025-08-28 19:26:23 +0200

    check and get user from clerk and save to database if not exists

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,10 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  const user = await checkUser();
+  console.log(user);
+
   return (
     <nav className="navbar">
       <div className="flex w-screen justify-around md:justify-between p-4 bg-zinc-950">
@@ -8,7 +12,7 @@ const Header = () => {
         <div>
           <SignedOut>
             <div className="bg-purple-900 py-1 px-4 broder rounded-md">
-            <SignInButton />
+              <SignInButton />
             </div>
           </SignedOut>
           <SignedIn>
--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-// Prevent multiple instances of Prisma Client in development
-declare global {
-  // Allow `globalThis.prisma` to exist
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-// Use existing client if available, otherwise create new
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "error", "warn"], // optional for debugging
+  });
 
-// Only assign to global in non-production
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
-
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
